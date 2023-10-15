cf1fd19b9b82a705660622895741c2a5ad8c2891
Dillon Williams <dillonwilliams2001@yahoo.com>
2023-10-15 17:54:59 -0500

    Added dynamic room creation on user join

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,57 @@
 import Room from "./datastructures/Room";
+import User from "./datastructures/User";
+import { GetChatInfoFromUrl } from "./domain/usecase/GetChatInfoFromUrl";
 
-const chatRoom = new Room();
-console.log(chatRoom.getId());
 
-const server = Bun.serve<{username: string}>({
-  websocket: {
-    open(ws) {
-      ws.subscribe(chatRoom.getId());
-      server.publish(chatRoom.getId(), `${ws.data.username} has entered the chat`);
-    },
-    message(ws, message) {
-      server.publish(chatRoom.getId(), `${ws.data.username}: ${message}`);
-    },
-    close(ws) {
-      ws.unsubscribe(chatRoom.getId());
-      server.publish(chatRoom.getId(), `${ws.data.username} has left the chat`);
-    },
-  },
+const rooms: Room[] = [];
+const getChatInfoFromUrl = new GetChatInfoFromUrl();
+
+const server = Bun.serve<{username: string, roomId: string}>({
   fetch(req, server) {
     const url = new URL(req.url);
-    const username = url.searchParams.get('username');
-    if (!username) {
-      return new Response("User name required", { status: 401 });
+    const response = getChatInfoFromUrl.get(url);
+    if (!response.getIsValid()) {
+      return new Response("Invalid HTTP Request", { status: 401 });
     }
 
-    const successfulUpgrade = server.upgrade(req, {data : { username }});
+    const username = response.getUsername();
+    const roomId = response.getRoomId();
+    const successfulUpgrade = server.upgrade(req, {data : { username, roomId }});
     return successfulUpgrade
       ? undefined
       : new Response("Web Socket upgrade error", {status: 400});
   },
+  websocket: {
+    open(ws) {
+      const newUser = new User();
+      newUser.setDisplayName(ws.data.username);
+
+      let foundRoom = rooms.find((room: Room) => room.getId() === ws.data.roomId);
+      if (!foundRoom) {
+        console.log('Creating new room...');
+        const newRoom = new Room();
+        newRoom.setId(ws.data.roomId);
+        newRoom.addUser(newUser);
+
+        rooms.push(newRoom);
+        foundRoom = newRoom;
+        console.log(foundRoom);
+        console.log(rooms);
+      } else {
+        foundRoom.addUser(newUser);
+      }
+      
+      ws.subscribe(foundRoom.getId());
+      server.publish(foundRoom.getId(), `${ws.data.username} has entered the chat`);
+    },
+    message(ws, message) {
+      server.publish(ws.data.roomId, `${ws.data.username}: ${message}`);
+    },
+    close(ws) {
+      ws.unsubscribe(ws.data.roomId);
+      server.publish(ws.data.roomId, `${ws.data.username} has left the chat`);
+    },
+  }
 });
 
 console.log(`Listening on ${server.hostname}:${server.port}`);
